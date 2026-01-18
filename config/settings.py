5f1759232dc9c520817b21ccd4bfc89f0e78f34b
GreatDe000 <Greatde@yandex.ru>
2026-01-19 00:27:07 +0300

    fix settings.py

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-4v75hfa!kuzs+m+s#p@3@55t&^+i1zoyn$sqnp=(qjfdq(g$qx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
 
 
 # Application definition
